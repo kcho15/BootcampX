da9016cd3cdc8e672014bec79c451364231051f4
Kevin Cho <kevcho604@gmail.com>
2023-04-04 13:06:54 -0700

    Add new tables for teachers and assistance requests

--- 3_queries_group-by/3_average_duration.sql
+++ 3_queries_group-by/3_average_duration.sql
@@ -3,4 +3,4 @@ FROM students
 JOIN assignment_submissions ON student_id = students.id
 WHERE end_date IS NULL
 GROUP BY student
-ORDER BY average_assignment_duration DESC;
+ORDER BY average_assignment_duration DESC;           
