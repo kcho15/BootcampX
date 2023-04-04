3f4c3dbd615041f54a7cfe65c2aab03f96903bcd
Kevin Cho <kevcho604@gmail.com>
2023-04-04 09:46:03 -0700

    Add GROUP BY queries

---
+++ 1_queries/1_rollover_students.sql
@@ -0,0 +1,8 @@
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date; 
+
+-- An INNER JOIN will only return results where there is a match between the two tables.
+-- An OUTER JOIN will do the same as an INNER JOIN but also returns unmatched rows in one or both tables. An OUTER JOIN can be LEFT, RIGHT, or FULL.
